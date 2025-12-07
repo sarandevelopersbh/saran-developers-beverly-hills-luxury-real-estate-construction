1ba157af3634efc929f7bbf93b748987c98a073a
sarandevelopersbh <sarandevelopersbh@gmail.com>
2025-12-06 19:05:44 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App.jsx';
+import '@/index.css';
+
+// ONLY KEEP THE CODE NECESSARY FOR REACT QUERY
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-) 
+  </QueryClientProvider>
+);
