f50ca9da06e5dbb0f6f6470e10ba449564419a98
sarandevelopersbh <sarandevelopersbh@gmail.com>
2025-12-08 00:36:29 -0800

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
 import ProjectCard from '../components/ProjectCard';
 import { Search, Filter, X } from 'lucide-react';
